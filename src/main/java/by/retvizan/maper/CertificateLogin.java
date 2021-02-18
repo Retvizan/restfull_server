e7e30e86b12a0e74654436074cba286764fe8809
Andrei Shkarubski <a.shkarubski@sap.com>
2021-02-18 19:54:49 +0300

    added logger

--- src/main/java/by/retvizan/maper/CertificateLogin.java
+++ src/main/java/by/retvizan/maper/CertificateLogin.java
@@ -1,9 +1,14 @@
 package by.retvizan.maper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class CertificateLogin {
-    private static final String IMPORT_TEXT = "import";
+    private static final Logger LOGGER = LoggerFactory.getLogger(CertificateLogin.class);
 
     private CertificateLogin() {
         throw new IllegalStateException("Utility class");
@@ -17,28 +22,27 @@ public class CertificateLogin {
      */
     public static void processCert(String certCommand, String userId) {
         String line;
-        String command = "remove".equals(certCommand) ? String.format("powershell.exe \"C:\\certs\\%s.ps1\" ", certCommand) :
-                String.format("powershell.exe \"C:\\certs\\%1$s.ps1 %2$s.pfx\"", certCommand, userId);
+        String command = "remove".equals(certCommand) ? String.format("powershell.exe \"C:\\certs\\%s.ps1\" ",
+                certCommand) : String.format("powershell.exe \"C:\\certs\\%1$s.ps1 %2$s.pfx\"", certCommand, userId);
         try {
             Process powerShellProcess = Runtime.getRuntime().exec(command);
             powerShellProcess.getOutputStream().close();
+
+            LOGGER.info("Standard Output:");
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
+            while ((line = stdout.readLine()) != null) {
+                LOGGER.info(line);
+            }
+            stdout.close();
+            LOGGER.info("Standard Error:");
+            BufferedReader stderr = new BufferedReader(new InputStreamReader(powerShellProcess.getErrorStream()));
+            while ((line = stderr.readLine()) != null) {
+                LOGGER.info(line);
+            }
+            stderr.close();
+            LOGGER.info("Done");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //LOGGER.info("Standard Output:");
-       /* BufferedReader stdout = new BufferedReader(new InputStreamReader(
-                powerShellProcess.getInputStream()));
-       *//* while ((line = stdout.readLine()) != null) {
-            LOGGER.info(line);
-        }*//*
-        stdout.close();
-        LOGGER.info("Standard Error:");
-        BufferedReader stderr = new BufferedReader(new InputStreamReader(
-                powerShellProcess.getErrorStream()));
-        while ((line = stderr.readLine()) != null) {
-            LOGGER.info(line);
-        }
-        stderr.close();
-        LOGGER.info("Done");*/
     }
 }
