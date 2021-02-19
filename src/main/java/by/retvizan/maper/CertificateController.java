b0b24df8ce033772c263920384f4c9814eafb050
Andrei Shkarubski <a.shkarubski@sap.com>
2021-02-19 10:18:15 +0300

    logger update

--- src/main/java/by/retvizan/maper/CertificateController.java
+++ src/main/java/by/retvizan/maper/CertificateController.java
@@ -1,5 +1,7 @@
 package by.retvizan.maper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,11 @@ import java.io.IOException;
 
 @RestController
 public class CertificateController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CertificateController.class);
     @GetMapping("/import")
     public String importCert(@RequestParam(value = "id", defaultValue = " ") String id) {
         CertificateLogin.processCert("import", id);
+        LOGGER.info("Certificate for user {} has been loaded", id);
         return String.format("Certificate of user %s has been loaded", id);
     }
 
